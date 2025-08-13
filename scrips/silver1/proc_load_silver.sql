e0f5bc01155395afd96ec52d9147fd1cb63810e2
SHAFEY SIDDIQUI <144645990+SHAFEY123@users.noreply.github.com>
2025-08-13 11:39:17 +0300

    Update proc_load_silver.sql

--- scrips/silver1/proc_load_silver.sql
+++ scrips/silver1/proc_load_silver.sql
@@ -219,18 +219,18 @@ FROM bronze.erp_cust_az12;
 
     PRINT '>> Inserting Data Into: silver.erp_loc_a101';
     INSERT INTO silver.erp_loc_a101 (
-        cid,
-        cntry
-    )
-    SELECT
-        REPLACE(cid, '', '') AS cid,
-        CASE
-            WHEN TRIM(cntry) = 'DE' THEN 'Germany'
-            WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
-            WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
-            ELSE TRIM(cntry)
-        END AS cntry
-    FROM bronze.erp_loc_a101;
+    cid,
+    cntry
+)
+SELECT
+    REPLACE(cid, '-', '') AS cid,  
+    CASE
+        WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+        WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+        WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+        ELSE TRIM(cntry)
+    END AS cntry
+FROM bronze.erp_loc_a101;
 
     set @end_time=GETDATE();
         print'>> Load duration: ' + cast(datediff(second, @start_time,@end_time) as nvarchar) +' seconds'
